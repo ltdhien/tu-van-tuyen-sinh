ccea20ff4df10ec6ee375b1fc386197d5a093fb4
ltdhien <ltdhien.hotmail.com>
2021-10-26 03:41:34 +0700

    make it better

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,7 +6,7 @@ let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
     router.get("/webhook", homeController.getWebhook);
     router.post("/webhook", homeController.postWebhook);
-    return app.use("/", router)
+    return app.use("/", router);
 }
 
 module.exports = initWebRoutes;
