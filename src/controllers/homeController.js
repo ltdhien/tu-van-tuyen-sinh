dcec30b9a500595a336b513e4d4fa61edb31f33e
ltdhien <ltdhien.hotmail.com>
2021-10-27 02:01:34 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -151,7 +151,7 @@ function callSendAPI(sender_psid, response) {
 
     // Send the HTTP request to the Messenger Platform
     request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "uri": "https://graph.facebook.com/v12.0/me/messages",
         "qs": { "access_token": ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
---
+++ src/services/chatbotService.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+import request from "request";
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+
+let callSendAPI = () => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v12.0/me/messages",
+        "qs": { "access_token": ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+let handleGetStarted = () => {
+
+}
+
+module.exports = {
+    handleGetStarted:handleGetStarted,
+}
