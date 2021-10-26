c83d96b4f9295b1883cab475af28f710e98d0698
ltdhien <ltdhien.hotmail.com>
2021-10-27 01:56:42 +0700

    make it better

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -120,16 +120,21 @@ function handlePostback(sender_psid, received_postback) {
     let payload = received_postback.payload;
 
     // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
-    } else if (payload === "GET_STARTED") {
-        response = {
-            "text": "OK. Xin chào mừng bạn ABC đến với tư vấn tuyển sinh trường Đại học Cần Thơ" 
-        }
+    switch(payload) {
+        case 'yes': 
+            response = {"text": "Thanks!"}
+            break;
+        case 'no':
+            response = { "text": "Oops, try sending another image." }
+            break;
+        case 'GET_STARTED':
+            response = {
+                "text": "OK. Xin chào mừng bạn ABC đến với tư vấn tuyển sinh trường Đại học Cần Thơ"
+            }
+            break;
+        default:
+            response = { "text": `Opp! I don't know response with postback ${payload}` }
     }
-
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
