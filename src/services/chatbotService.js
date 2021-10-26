4d7ddeff865e20112ce6e558f239be2e283ce8d8
ltdhien <ltdhien.hotmail.com>
2021-10-27 04:22:30 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -4,6 +4,8 @@ import request from "request";
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 
+const ImageGetStarted = `https://bit.ly/get-started-chatbot-by-hien`
+
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
     let request_body = {
@@ -51,10 +53,18 @@ let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
             let username = await getUserName(sender_psid);
-            let response = {
+            let response1 = {
                 "text": `OK. Xin chào mừng bạn ${username} đến với tư vấn tuyển sinh trường Đại học Cần Thơ`
             }
-            await callSendAPI(sender_psid, response);
+            let response2 = sendGetStartedTemplate();
+            //send message
+            await callSendAPI(sender_psid, response1);
+
+            //send generic template message
+            await callSendAPI(sender_psid, response2);
+
+
+
             resolve('done');
         } catch (e) {
             reject(e);
@@ -62,6 +72,39 @@ let handleGetStarted = (sender_psid) => {
     })
 }
 
+let sendGetStartedTemplate = () => {
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Xin chào bạn đến với tư vấn tuyển sinh của Hiền",
+                    "subtitle": "Dưới đây là các lựa chọn của tư vấn",
+                    "image_url": ImageGetStarted,
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "Điểm tuyển sinh",
+                            "payload": "ADMISSION_SCORE",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Ngành tuyển sinh",
+                            "payload": "MAJOR",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Chỉ tiêu",
+                            "payload": "TARGET",
+                        }
+                    ],
+                }]
+            }
+        }
+    }
+    return ``;
+}
 
 module.exports = {
     handleGetStarted: handleGetStarted,
