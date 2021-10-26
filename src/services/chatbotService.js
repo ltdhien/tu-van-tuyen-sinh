08b77f62dd68112081ed52bf0aed24ac9e3d1331
ltdhien <ltdhien.hotmail.com>
2021-10-27 03:32:58 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import { response } from "express";
 import request from "request";
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
@@ -27,11 +28,32 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
+let getUserName = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${ACCESS_TOKEN}`,
+        "qs": { "access_token": ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log(body);
+        if (!err) {
+            response = JSON.parse(res);
+            username = `${response.first_name} ${response.last_name}`;
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+
+    return username;
+}
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
+            let username = await getUserName(sender_psid);
             let response = {
-                "text": "OK. Xin chào mừng bạn ABC đến với tư vấn tuyển sinh trường Đại học Cần Thơ"
+                "text": `OK. Xin chào mừng bạn ${username} đến với tư vấn tuyển sinh trường Đại học Cần Thơ`
             }
             await callSendAPI(sender_psid, response);
             resolve('done');
@@ -44,4 +66,5 @@ let handleGetStarted = (sender_psid) => {
 
 module.exports = {
     handleGetStarted: handleGetStarted,
+    getUserName: getUserName,
 }
